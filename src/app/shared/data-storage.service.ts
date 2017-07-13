d5f7b6c5cef775e12e44bb5abbd1eb4e8b2a59e9
Alberto Ferrini <alberto.ferrini.mail@gmail.com>
2017-07-13 11:01:16 +0200

    lesson 254 finished

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,17 +5,22 @@ import { Recipe } from '../recipes/recipe.model';
 
 import 'rxjs/Rx'
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http,
+        private recipeService: RecipeService,
+        private authService: AuthService) { }
 
     storeRecipes() {
-        return this.http.put('https://udemy-ng4-recipes.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://udemy-ng4-recipes.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        return this.http.get('https://udemy-ng4-recipes.firebaseio.com/recipes.json').map(
+        const token = this.authService.getToken();
+        return this.http.get('https://udemy-ng4-recipes.firebaseio.com/recipes.json?auth=' + token).map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
                 for (let recipe of recipes) {
