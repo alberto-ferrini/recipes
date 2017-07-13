8c1f1c8b492b741118a6799244599343222dae34
Alberto Ferrini <alberto.ferrini.mail@gmail.com>
2017-07-13 10:02:47 +0200

    lesson 250 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature='recipe';
-  onNavigate(feature:string){
-    this.loadedFeature=feature;
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCePediLVFYWeCx1KaqnIgGQAs-gjjFcNE',
+      authDomain: 'udemy-ng4-recipes.firebaseapp.com'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
